21355a7930c06a7a1e4d769d85909c4278c15fda
lucaseliel <lucaseliel.business@gmail.com>
2023-01-12 09:49:27 -0300

    Example of Single Principle Responsability

--- src/main/java/without_srp/Student.java
+++ src/main/java/without_srp/Student.java
@@ -1,6 +1,5 @@
 package without_srp;
 
-
 //This class is executing several commands for one only responsibility.
 // Then, we should refactor this class and separate the responsibilities
 public class Student {
